224cff00bc9f34564039b9ca9b41e93f22d4c03f
DigoFernandes <digofernandes017@gmail.com>
2026-01-23 19:34:04 -0300

    first commit

---
+++ src/main/java/rodrigo/ucsal/dev/apiwishlist/ApiWishlistApplication.java
@@ -0,0 +1,13 @@
+package rodrigo.ucsal.dev.apiwishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rodrigo/ucsal/dev/apiwishlist/ApiWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package rodrigo.ucsal.dev.apiwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
